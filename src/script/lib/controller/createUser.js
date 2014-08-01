fa81ba753ba9ecd7c7b7cce93f4f8a9c3064ea53
schinken <schinken@hackerspace-bamberg.de>
2014-08-01 21:43:41 +0200

    Remove autofocus; Disable autocomplete

--- src/script/lib/controller/createUser.js
+++ src/script/lib/controller/createUser.js
@@ -1,6 +1,8 @@
 module.exports.install = function(app) {
     app.controller('CreateUserController', function ($scope, locationService, userService) {
 
+        $scope.name.focus();
+
         $scope.createUser = function() {
             userService
                 .createUser($scope.name)
