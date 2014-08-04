20104e213bd3b947512cfe5431bcec12b0736857
schinken <schinken@hackerspace-bamberg.de>
2014-08-04 21:58:18 +0200

    Don't minify app, just external sources #17

--- gulpfile.js
+++ gulpfile.js
@@ -97,8 +97,6 @@ gulp.task('scripts_app', function () {
     return gulp
         .src(SOURCE_DIR + '/script/app.js')
         .pipe(browserify()).on('error', gutil.log)
-        .pipe(ngannotate())
-        .pipe(gulpIf(isProduction, uglify()))
         .pipe(gulp.dest(TARGET_DIR + '/js'));
 });
 
