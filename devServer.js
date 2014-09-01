e7b6b87737009f609174881fc94bff31a48b3151
zaphod1984 <zaphod84@gmx.de>
2014-09-01 09:29:34 +0200

    devServer update
    * instruments the listen callback
    * figlet magic

--- devServer.js
+++ devServer.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var args = require('yargs').argv;
+var figlet = require('figlet');
 
 var PORT = args.port || 8081;
 
+console.log(figlet.textSync('Strichliste-Web Dev'));
+
 express()
     .use(express.static(__dirname + '/build'))
-    .listen(PORT);
-
-console.log('running on: ' + PORT);
+    .listen(PORT, function() {
+        console.log('running on: ' + PORT);
+    });
