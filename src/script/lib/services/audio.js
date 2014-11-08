b5077cb302d273b6f2fe31ef7b94dec001e4fdb7
schinken <schinken@hackerspace-bamberg.de>
2014-11-08 16:09:25 +0100

    #26 Use  instead of XMLHttpRequest

--- src/script/lib/services/audio.js
+++ src/script/lib/services/audio.js
@@ -2,25 +2,23 @@ var angular = require('../../lib/angular');
 
 var context = new (window.AudioContext || window.webkitAudioContext)();
 
-function AudioService() {
+function AudioService($http) {
 
     this.prefetchedAudioObjects = {};
 
     this.prefetch = function(filename) {
 
         var that = this;
-        var request = new XMLHttpRequest();
 
-        request.open("GET", filename, true);
-        request.responseType = 'arraybuffer';
-
-        request.onload = function() {
-            context.decodeAudioData(request.response, function(buffer) {
-                that.prefetchedAudioObjects[filename] = buffer;
+        $http
+            .get(filename, {
+                responseType: 'arraybuffer'
+            })
+            .success(function(data) {
+                context.decodeAudioData(data, function(buffer) {
+                    that.prefetchedAudioObjects[filename] = buffer;
+                });
             });
-        };
-
-        request.send();
     };
 
     this.play = function(filename) {
